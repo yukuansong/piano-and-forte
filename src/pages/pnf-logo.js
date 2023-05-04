c8ed7bcbb073241ef90d9f3afe7ea203fe6c406b
yukuansong <yukuan.song@yahoo.com>
2023-05-04 15:32:18 -0400

    background-color

--- src/pages/pnf-logo.js
+++ src/pages/pnf-logo.js
@@ -3,7 +3,11 @@ import "./pnf-logo.css"
 function PnfLogo() {
     return (
         <div className="pnf-container">
-            <div className="pnf-circle"></div>
+            <div className="pnf-circle">
+                <div className="pnf-base-container">
+                    <div className="pnf-base-items">Piano ands Forte</div>
+                </div>
+            </div>
             <div className="pnf-keys">
                 <div className="key white"></div>
                 <div className="key black"></div>
