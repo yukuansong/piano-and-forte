5815ecd6da856a6fd9917851bcfb2de0ed4b7fb2
yukuansong <yukuan.song@yahoo.com>
2023-06-07 08:43:23 -0400

    signup background

--- src/pages/createUser.js
+++ src/pages/createUser.js
@@ -2,7 +2,9 @@ import "./createUser.css";
 
 import { useState } from "react";
 
-import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+import "../../node_modules/font-awesome/css/font-awesome.min.css";
 
 function CreateUser(prep) {
   const [input, setInput] = useState({
@@ -36,8 +38,8 @@ function CreateUser(prep) {
         case "email":
           if (!value) {
             stateObj[name] = "Please enter Email";
-          } else if (!(/\S+@\S+\.\S+/.test(value))) {
-            stateObj[name] = "Email is invalid"
+          } else if (!/\S+@\S+\.\S+/.test(value)) {
+            stateObj[name] = "Email is invalid";
           }
           break;
         case "password":
@@ -77,48 +79,68 @@ function CreateUser(prep) {
       .catch((error) => {
         // Error handling
         alert(error.message);
-      })
+      });
   };
 
   return (
     <div className="create-user-container">
-      <input
-        onChange={onInputChange}
-        onBlur={validateInput}
-        name="email"
-        value={input.email}
-        className="input-field"
-        type="email"
-        placeholder="Email Address"
-      />
-      {
-        error.email && <span className="err">{error.email}</span>
-      }
-      <input
-        onChange={onInputChange}
-        onBlur={validateInput}
-        name="password"
-        value={input.password}
-        className="input-field"
-        type="password"
-        placeholder="Password"
-      />
-      {
-        error.password && <span className="err">{error.password}</span>
-      }
-      <input
-        onChange={onInputChange}
-        onBlur={validateInput}
-        name="confirmPassword"
-        value={input.confirmPassword}
-        className="input-field"
-        type="password"
-        placeholder="Confirmed Password"
-      />
-      {
-        error.confirmPassword && <span className="err">{error.confirmPassword}</span>
-      }
-      <button onClick={handleSubmit} className="create-button">Register</button>
+      <h1>SIGN UP</h1>
+
+      <div className="signup-icon">
+        <i className="fa fa-user-circle"></i>
+      </div>
+      <div className="registration-input">
+        <label htmlFor="email" className="registration-label">
+          <i className="fa fa-envelope" id="registration-icon"></i>
+        </label>
+        <input
+          onChange={onInputChange}
+          onBlur={validateInput}
+          name="email"
+          id="email"
+          value={input.email}
+          className="input-field"
+          type="email"
+          placeholder="Email Address"
+        />
+      </div>
+      {error.email && <span className="err">{error.email}</span>}
+      <div className="registration-input">
+        <label htmlFor="password" className="registration-label">
+          <i className="fa fa-unlock-alt" id="registration-icon"></i>
+        </label>
+        <input
+          onChange={onInputChange}
+          onBlur={validateInput}
+          name="password"
+          id="password"
+          value={input.password}
+          className="input-field"
+          type="password"
+          placeholder="Password"
+        />
+      </div>
+      {error.password && <span className="err">{error.password}</span>}
+      <div className="registration-input">
+        <label htmlFor="confirm-password" className="registration-label">
+          <i className="fa fa-unlock-alt" id="registration-icon"></i>
+        </label>
+        <input
+          onChange={onInputChange}
+          onBlur={validateInput}
+          name="confirmPassword"
+          value={input.confirmPassword}
+          className="input-field"
+          type="password"
+          placeholder="Confirmed Password"
+        />
+      </div>
+      {error.confirmPassword && (
+        <span className="err">{error.confirmPassword}</span>
+      )}
+      <button onClick={handleSubmit} className="create-button">
+        Register
+      </button>
     </div>
   );
 }
